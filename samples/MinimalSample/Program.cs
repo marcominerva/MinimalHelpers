ec524a3fce44d2a077a6601d7ffc2960e5dd6e7b
Marco Minerva <marco.minerva@gmail.com>
2024-08-01 14:50:37 +0200

    Refactor Swagger configuration and update dependencies
    Updated `MinimalSample.csproj` to use `Swashbuckle.AspNetCore` version `6.7.0`. Removed custom Swagger operation filters and related code from `Program.cs`, `FormFileOperationFilter.cs`, and `MissingSchemasOperationFilter.cs`. Updated `MinimalHelpers.OpenApi.csproj` to target only `net8.0` and upgraded `Swashbuckle.AspNetCore.SwaggerGen` to `6.7.0`. Removed `AddMissingSchemas` method and marked `AddFormFile` as obsolete in `SwaggerExtensions.cs`. Cleaned up unnecessary using directives and conditional compilation directives.

--- samples/MinimalSample/Program.cs
+++ samples/MinimalSample/Program.cs
@@ -1,16 +1,7 @@
-using MinimalHelpers.OpenApi;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddFormFile();
-
-    // This is only needed with .NET 7.0.
-    //options.AddMissingSchemas();
-});
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -25,27 +16,5 @@ if (app.Environment.IsDevelopment())
 
 // Maps all the endpoints within this Assembly.
 app.MapEndpoints();
-
-app.MapPost("/api/upload", (IFormFile file) =>
-{
-    return TypedResults.Ok(new { file.FileName, file.ContentType, file.Length });
-})
-.DisableAntiforgery()
-.WithOpenApi(operation =>
-{
-    operation.Description = "If you use the 'WithOpenApi' extension method, you need to call the 'AddFormFile' method on the 'SwaggerGenOptions' instance to be sure that swagger.json file contains the right definition.";
-    return operation;
-});
-
-app.MapPost("/api/multiupload", (IFormFileCollection files) =>
-{
-    return TypedResults.Ok(new { FileCount = files.Count });
-})
-.DisableAntiforgery()
-.WithOpenApi(operation =>
-{
-    operation.Description = "If you use the 'WithOpenApi' extension method, you need to call the 'AddFormFile' method on the 'SwaggerGenOptions' instance to be sure that swagger.json file contains the right definition.";
-    return operation;
-});
 
 app.Run();
