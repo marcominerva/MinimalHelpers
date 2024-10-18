0cf9c70767404cbad044ee16543d235434955d73
Marco Minerva <marco.minerva@gmail.com>
2024-10-18 15:33:57 +0200

    Add ProducesDefaultProblem method to RouteHandlerBuilder #64
    Introduce a new extension method `ProducesDefaultProblem` for the
    `RouteHandlerBuilder` class to simplify defining common error
    responses.
    
    Enhance `README.md` with
    documentation and examples for the new method.

---
+++ src/MinimalHelpers.OpenApi/RouteHandlerBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MinimalHelpers.OpenApi;
+
+/// <summary>
+/// Extension methods for <see cref="RouteHandlerBuilder"/>.
+/// </summary>
+/// <seealso cref="RouteHandlerBuilder"/>
+public static class RouteHandlerBuilderExtensions
+{
+    /// <summary>
+    /// Adds to <see cref="RouteHandlerBuilder"/> the specified list of status codes as <see cref="ProblemDetails"/> responses.
+    /// </summary>
+    /// <param name="builder">The <see cref="RouteHandlerBuilder"/>.</param>
+    /// <param name="statusCodes">The list of status codes to be added as <see cref="ProblemDetails"/> responses.</param>
+    /// <returns>The <see cref="RouteHandlerBuilder"/> with the new status codes responses.</returns>
+    public static RouteHandlerBuilder ProducesDefaultProblem(this RouteHandlerBuilder builder, params int[] statusCodes)
+    {
+        foreach (var statusCode in statusCodes)
+        {
+            builder.ProducesProblem(statusCode);
+        }
+
+        return builder;
+    }
+}
